2122d1ec11167af5acd173acb981ac44e1141a32
Phani Sai <42872894+phanisai22@users.noreply.github.com>
2019-07-17 21:01:41 +0530

    Add files via upload

---
+++ Python/Loops.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    if 1 <= n <= 20:
+        for i in range(0, n):
+            print(i * i)
---
+++ Python/Python Division.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(str(int(a / b)) + "\n" + str(a / b))
